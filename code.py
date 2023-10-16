3d391c3931484dd74e522e13a1515d73bc7da5f0
rs-wal-rd <148042830+rs-wal-rd@users.noreply.github.com>
2023-10-16 16:21:55 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,28 @@
-Hiiii
-test1
-ahjdf
-asyjhf
-jfh
-jsakdgsvksdcs
-skjsjcscfscvjkdscv
-123455
-walnutmedical
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin for the LED
+led_pin = 17
+
+# Set up the GPIO pin for the LED as an output
+GPIO.setup(led_pin, GPIO.OUT)
+
+try:
+    while True:
+        # Turn the LED on
+        GPIO.output(led_pin, GPIO.HIGH)
+        print("LED is ON")
+        time.sleep(1)  # Wait for 1 second
+
+        # Turn the LED off
+        GPIO.output(led_pin, GPIO.LOW)
+        print("LED is OFF")
+        time.sleep(1)  # Wait for 1 second
+
+except KeyboardInterrupt:
+    # Turn off the LED and clean up the GPIO pins on program exit
+    GPIO.output(led_pin, GPIO.LOW)
+    GPIO.cleanup()
